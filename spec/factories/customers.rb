c4ad111152be7029296b1fc9601257eab6770391
Michael Cooper <macoop2363@gmail.com>
2019-11-19 14:52:20 -0700

    Add dynamic name generation for customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :customer do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    sequence :first_name do |n|
+      "first_name_#{n}"
+    end
+    sequence :last_name do |n|
+      "last_name_#{n}"
+    end
   end
 end
