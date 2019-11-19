866bde7980a2feeea645aab30cec4a519f7bf34d
Michael Cooper <macoop2363@gmail.com>
2019-11-19 14:27:13 -0700

    Add validation and relationship testing for all current models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+
+  end
+
+  describe 'methods' do
+
+  end
 end
