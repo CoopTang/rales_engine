3dd504933f0c9d96f5a4a57d9a1532a1a3909147
Michael Cooper <macoop2363@gmail.com>
2019-11-20 11:58:32 -0700

    Add test for merchant show endpoint and rewind FactoryBot sequences every test

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API:" do
+  before :each do
+    FactoryBot.rewind_sequences
+  end
+
   it 'Index' do
     create_list(:merchant, 2)
 
@@ -14,5 +18,15 @@ RSpec.describe "Merchants API:" do
 
     expect(merchants["data"][0]["attributes"]["name"]).to eq("merchant_1")
     expect(merchants["data"][1]["attributes"]["name"]).to eq("merchant_2")
+  end
+
+  it 'Show' do
+    create_list(:merchant, 2)
+
+    get "/api/v1/merchants/#{Merchant.last.id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["data"]["attributes"]["name"]).to eq("merchant_2")
   end
 end
