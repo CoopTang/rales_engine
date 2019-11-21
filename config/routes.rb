abe800468b6e1c6e67a9d04a000088d1b3fe52e0
Michael Cooper <macoop2363@gmail.com>
2019-11-21 16:26:00 -0700

    Scope Merchant resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index]
-        resources :invoices, only: [:index]
-        collection do
-          get 'find', to: 'merchants#show'
-          get 'find_all'
-          get 'random'
+      scope module: 'merchants' do
+        resources :merchants, only: [:index, :show] do
+          resources :items, only: [:index]
+          resources :invoices, only: [:index]
+          collection do
+            get 'find', to: 'merchants#show'
+            get 'find_all'
+            get 'random'
+          end
         end
       end
     end
