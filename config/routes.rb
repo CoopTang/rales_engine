047a5af78277588754d4930a210159f3a992ec21
Michael Cooper <macoop2363@gmail.com>
2019-11-20 17:13:41 -0700

    Add routing for merchant find api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        collection do
+          get 'find', to: 'merchants#show'
+        end
+      end
     end
   end
 end
