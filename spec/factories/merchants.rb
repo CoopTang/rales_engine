1fb9c1675a73167c961c592f25cc8a62fd40546b
Michael Cooper <macoop2363@gmail.com>
2019-11-21 11:07:07 -0700

    Add new factory for a merchant with items

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :merchant do
-    sequence :name do |n|
-      "merchant_#{n}"
+    sequence(:name) { |n| "merchant_#{n}" }
+
+    factory :merchant_with_items do
+      sequence(:name) { |n| "merchant_#{n}" }
+      after :create do |merchant|
+        create_list(:item, 3, merchant: merchant)
+      end
     end
   end
 end
