ac9ba5c53aae323bc62d0eb26396d48f163f47c5
Michael Cooper <macoop2363@gmail.com>
2019-11-21 10:13:47 -0700

    Add dynamic name and description generation to Item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
+    sequence :name do |n|
+      "item_#{n}" }
+    sequence :desciption do |n|
+      "item_description_#{n}"
+    end
     unit_price { 1 }
     merchant { nil }
   end
