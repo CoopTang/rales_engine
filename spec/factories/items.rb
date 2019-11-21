d9819d7a5e6a55b331ec4bdf1e1f18fa405eb4d2
Michael Cooper <macoop2363@gmail.com>
2019-11-21 10:16:23 -0700

    Fix error in item factory

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :item do
     sequence :name do |n|
-      "item_#{n}" }
-    sequence :desciption do |n|
+      "item_#{n}"
+    end
+    sequence :description do |n|
       "item_description_#{n}"
     end
     unit_price { 1 }
