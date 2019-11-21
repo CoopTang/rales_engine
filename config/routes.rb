8db33c1f3ed94fd71d4dddb1b36f7f9b098c5f86
Michael Cooper <macoop2363@gmail.com>
2019-11-21 11:53:14 -0700

    Add nested resources routes to merchants

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index]
+        resources :invoices, only: [:index]
         collection do
           get 'find', to: 'merchants#show'
           get 'find_all'
