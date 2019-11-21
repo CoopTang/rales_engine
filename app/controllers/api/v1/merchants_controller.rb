ea2232a19a34d42f3e6faf34d859ae616843501c
Michael Cooper <macoop2363@gmail.com>
2019-11-20 17:14:31 -0700

    Add merchant search params private method and refactor show controller to use the private method

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,8 +6,14 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def show
-    merchant = Merchant.find_by(id: params[:id])
+    merchant = Merchant.find_by(merchant_search_params)
     render json: MerchantSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant_search_params
+    params.permit(:id, :name, :created_at, :updated_at)
   end
 
 end
