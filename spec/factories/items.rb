c9a4d8231e3693973822ed9d0c0fc8de93d1fcfd
Michael Cooper <macoop2363@gmail.com>
2019-11-27 14:25:35 -0700

    Add a factory for an item with invoice_items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,17 @@
 FactoryBot.define do
   factory :item do
-    sequence :name do |n|
-      "item_#{n}"
-    end
-    sequence :description do |n|
-      "item_description_#{n}"
-    end
-    sequence :unit_price do |n|
-      n
-    end
+    sequence(:name) { |n| "item_#{n}" }
+    sequence(:description) { |n| "item_description_#{n}" }
+    sequence(:unit_price) { |n| n }
     merchant
+
+    factory :item_with_invoices do
+      sequence(:name) { |n| "item_#{n}" }
+      sequence(:description) { |n| "item_description_#{n}" }
+      sequence(:unit_price) { |n| n }
+      after :create do |item|
+        create_list(:invoice_item, 3, item: item)
+      end
+    end
   end
 end
