3acd49af7aaf5dd4a143ca306e4d468f881fcc34
Michael Cooper <macoop2363@gmail.com>
2019-11-30 14:11:43 -0700

    Add factory to generate an invoice with transactions

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -2,6 +2,15 @@ FactoryBot.define do
   factory :invoice do
     customer
     merchant
-    status { "MyString" }
+    status { "success" }
+
+    factory :invoice_with_transactions do
+      customer
+      merchant
+      status { "success" }
+      after :create do |invoice|
+        create_list(:transaction, 3, invoice: invoice)
+      end
+    end
   end
 end
