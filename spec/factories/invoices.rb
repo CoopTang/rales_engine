1029e4604a544252f9426cc0a5fc242dc1ce93f2
Michael Cooper <macoop2363@gmail.com>
2019-11-30 15:55:00 -0700

    Add additional factories for invoices with invoice_items and items

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -2,14 +2,32 @@ FactoryBot.define do
   factory :invoice do
     customer
     merchant
-    status { "success" }
+    status { "shipped" }
 
     factory :invoice_with_transactions do
       customer
       merchant
-      status { "success" }
+      status { "shipped" }
       after :create do |invoice|
         create_list(:transaction, 3, invoice: invoice)
+      end
+    end
+
+    factory :invoice_with_items do
+      customer
+      merchant
+      status { "shipped" }
+      after :create do |invoice|
+        create_list(:item, 3, merchant: invoice.merchant)
+      end
+    end
+
+    factory :invoice_with_invoice_items do
+      customer
+      merchant
+      status { "shipped" }
+      after :create do |invoice|
+        create_list(:invoice_item, 3, invoice: invoice)
       end
     end
   end
