90ee4724dd4254ff73d265f39ec738606b36485d
Michael Cooper <macoop2363@gmail.com>
2019-12-01 14:14:33 -0700

    Add test for total_revenue Merchant class method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -10,6 +10,31 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'methods' do
+    before :each do
+      FactoryBot.rewind_sequences
+    end
 
+    describe 'total_revenue' do
+      before :each do
+        @merchant_1 = create(:merchant)
+        @merchant_item_1 = create(:item_with_transactions, merchant: @merchant_1)
+  
+        @merchant_2 = create(:merchant)
+        @merchant_item_2 = create(:item_with_transactions, merchant: @merchant_2, unit_price: 30)
+  
+        @merchant_3 = create(:merchant)
+        @merchant_item_3 = create(:item_with_transactions, merchant: @merchant_3)
+      end
+
+      it 'can get a variable number of top merchants' do
+        result = Merchant.total_revenue(3)
+
+        expect(result).to eq([@merchant_2, @merchant_3, @merchant_1])
+
+        result = Merchant.total_revenue(2)
+
+        expect(result).to eq([@merchant_2, @merchant_3])
+      end
+    end
   end
 end
