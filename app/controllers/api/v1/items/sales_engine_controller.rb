83163d526ec7954c6409a9e3cd376949766284a6
Michael Cooper <macoop2363@gmail.com>
2019-12-01 13:46:11 -0700

    Rename variable names due to changes in Item model

--- app/controllers/api/v1/items/sales_engine_controller.rb
+++ app/controllers/api/v1/items/sales_engine_controller.rb
@@ -4,4 +4,22 @@ class Api::V1::Items::SalesEngineController < ApplicationController
     items = Item.most_revenue(params[:quantity])
     render json: ItemSerializer.new(items)
   end
+
+  def best_day
+    invoice = Item.find_by(id: params[:id]).best_day_invoice
+    render json: best_day_serialized(invoice)
+  end
+
+  private
+
+  def best_day_serialized(invoice)
+    day = DateTime.parse(invoice.created_at.to_s).strftime("%Y-%m-%d")
+    {
+      data: {
+        attributes: {
+          best_day: day
+        }
+      }
+    }
+  end
 end
