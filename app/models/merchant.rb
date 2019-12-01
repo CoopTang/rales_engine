a780191465faf3406dcd00875c487bfbd7468b58
Michael Cooper <macoop2363@gmail.com>
2019-12-01 14:15:01 -0700

    Add Merchant class method to return the top number of merchants by total revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,13 @@ class Merchant < ApplicationRecord
   validates_presence_of :name
 
   has_many :items
+
+  def self.total_revenue(quantity = 1, date = nil)
+    select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+      .joins(items: [:invoice_items, invoices: [:transactions]])
+      .merge(Transaction.successful)
+      .group(:id)
+      .order('revenue DESC')
+      .limit(quantity)
+  end
 end
