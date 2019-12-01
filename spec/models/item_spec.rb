ca394f85c75fc639034888c64b7672879e4b999f
Michael Cooper <macoop2363@gmail.com>
2019-12-01 12:41:47 -0700

    Add test to return an Item's best InvoiceItem by quantity

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,5 +31,38 @@ RSpec.describe Item, type: :model do
       result = Item.most_revenue(3)
       expect(result).to eq([item_3, item_5, item_4])
     end
+
+    it '#best_day' do
+      item_1 = create(:item)
+      invoice_1 = create(:invoice, merchant: item_1.merchant)
+      invoice_item_1 = create(
+        :invoice_item,
+        invoice: invoice_1,
+        item: item_1,
+        unit_price: item_1.unit_price,
+        quantity: 4
+      )
+      invoice_item_2 = create(
+        :invoice_item,
+        invoice: invoice_1,
+        item: item_1,
+        unit_price: item_1.unit_price,
+        quantity: 5,
+        created_at: "2012-03-27 14:53:00 UTC"
+        
+      )
+      invoice_item_3 = create(
+        :invoice_item,
+        invoice: invoice_1,
+        item: item_1,
+        unit_price: item_1.unit_price,
+        quantity: 5,
+        created_at: "2012-03-26 14:53:59 UTC"
+      )
+
+      result = item_1.best_day
+
+      expect(result).to eq([invoice_item_3])
+    end
   end
 end
