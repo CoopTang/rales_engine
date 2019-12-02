0ce3334d71c92797ecf9f6a904f7309ae8295246
Michael Cooper <macoop2363@gmail.com>
2019-12-01 22:53:45 -0700

    Add additional through relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,5 +3,15 @@ class Customer < ApplicationRecord
                         :last_name
 
   has_many :invoices
+  has_many :merchants, through: :invoices
   has_many :transactions, through: :invoices
+
+  def favorite_merchant
+    merchants.select('merchants.*, COUNT(invoices.*) AS qty')
+      .joins(invoices: [:transactions])
+      .merge(Transaction.successful)
+      .group(:id)
+      .order('qty DESC')
+      .first
+  end
 end
